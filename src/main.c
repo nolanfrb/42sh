fb3c4ac0945770cf2b9f4ed049c2121e7bec3c0a
Lukas R. <lukas.renaud@epitech.eu>
2025-04-07 10:49:56 +0200

    feat: implement main function to print "Hello, World!"

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** clonerepo [WSL: Ubuntu-24.04]
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(int ac, char const **av)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
