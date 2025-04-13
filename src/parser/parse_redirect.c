ce1e79a4478a80118308ad0be58586154505d907
Nolan Fribault <nolan.fribault@epitech.eu>
2025-04-13 13:40:49 +0200

    feat(ast): add process_redirect_node function for improved redirection handling

--- src/parser/parse_redirect.c
+++ src/parser/parse_redirect.c
@@ -27,13 +27,16 @@ redirect_type_t get_redirection_type(const char *token)
         return REDIR_APPEND;
     if (strcmp(token, "<<") == 0)
         return REDIR_HEREDOC;
+    fprintf(stderr, "error: invalid redirection type\n");
     return REDIR_NONE;
 }
 
-ast_node_t *create_redir_node(ast_node_t *child, redirect_type_t type, char *filename)
+ast_node_t *create_redir_node(
+    ast_node_t *child, redirect_type_t type, char *filename
+)
 {
     ast_node_t *node = malloc(sizeof(ast_node_t));
-    
+
     if (!node) {
         perror("malloc");
         return NULL;
@@ -43,6 +46,30 @@ ast_node_t *create_redir_node(ast_node_t *child, redirect_type_t type, char *fil
     node->data.redir.type = type;
     node->data.redir.filename = filename;
     return node;
+}
+
+static int process_redirect_node(ast_node_t **node, char **tokens, int *pos)
+{
+    char *filename = NULL;
+    redirect_type_t redir_type = get_redirection_type(tokens[*pos]);
+    ast_node_t *redir = NULL;
+
+    if (redir_type == REDIR_NONE)
+        return -1;
+    (*pos)++;
+    if (!tokens[*pos]) {
+        fprintf(stderr, "error: missing filename for redirection\n");
+        return -1;
+    }
+    filename = tokens[*pos];
+    (*pos)++;
+    redir = create_redir_node(*node, redir_type, filename);
+    if (!redir) {
+        fprintf(stderr, "error: failed to create redirection node\n");
+        return -1;
+    }
+    *node = redir;
+    return 0;
 }
 
 /**
@@ -55,26 +82,12 @@ ast_node_t *create_redir_node(ast_node_t *child, redirect_type_t type, char *fil
 ast_node_t *parse_redirect(char **tokens, int *pos)
 {
     ast_node_t *node = parse_subshell(tokens, pos);
-    redirect_type_t redir_type;
-    ast_node_t *redir = NULL;
-    char *filename = NULL;
 
     if (!node)
         return NULL;
     while (tokens[*pos] && is_redirect_op(tokens[*pos])) {
-        redir_type = get_redirection_type(tokens[*pos]);
-        (*pos)++;
-        
-        if (!tokens[*pos]) {
-            fprintf(stderr, "error: missing filename for redirection\n");
+        if (process_redirect_node(&node, tokens, pos) == -1)
             return NULL;
-        }
-        filename = tokens[*pos];
-        (*pos)++;
-        redir = create_redir_node(node, redir_type, filename);
-        if (!redir)
-            return NULL;
-        node = redir;
     }
     return node;
 }
