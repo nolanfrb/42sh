d8878db0f51d9dc2675604f1052515b1561f5c27
LoannBi <loann.bideau@epitech.eu>
2025-04-14 23:38:58 +0200

    test(count_words): remove commented code from special tokens tests

--- tests/lexer/test_count_words.c
+++ tests/lexer/test_count_words.c
@@ -22,14 +22,14 @@ Test(count_words, should_handle_multiple_spaces)
 
 Test(count_words, should_count_special_tokens_as_words)
 {
-    cr_assert_eq(count_words("echo && ls", " "), 3); // echo, &&, ls
-    cr_assert_eq(count_words("a||b", " "), 3);       // a, ||, b
-    cr_assert_eq(count_words("x|y|z", " "), 5);      // x, |, y, |, z
+    cr_assert_eq(count_words("echo && ls", " "), 3);
+    cr_assert_eq(count_words("a||b", " "), 3);
+    cr_assert_eq(count_words("x|y|z", " "), 5);
 }
 
 Test(count_words, should_mix_words_and_special_tokens)
 {
-    cr_assert_eq(count_words("grep foo | wc -l", " "), 5); // grep, foo, |, wc, -l
+    cr_assert_eq(count_words("grep foo | wc -l", " "), 5);
     cr_assert_eq(count_words("cmd1 && cmd2 || cmd3", " "), 5);
 }
 
