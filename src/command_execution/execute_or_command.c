583293fd4d0bfcbca31932d2bc1d75cd0c357791
Yannislrg <yannis.lorang@epitech.eu>
2025-04-15 10:46:58 +0200

    fix(command): ensure execute_or returns -1 when both commands fail

--- src/command_execution/execute_or_command.c
+++ src/command_execution/execute_or_command.c
@@ -12,4 +12,5 @@ int execute_or(ast_node_t *ast, shell_t *shell_info)
 {
     if (process_command(ast->data.binop.left, shell_info) != 0)
         return process_command(ast->data.binop.right, shell_info);
+    return -1;
 }
