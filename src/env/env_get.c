62a11ea369460c9d6b22974d16b646657ca3d77d
LoannBi <loann.bideau@epitech.eu>
2025-04-15 14:48:22 +0200

    feat(env): add get_env_value function to retrieve environment variable values

---
+++ src/env/env_get.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** minishell_project
+** File description:
+** env_get
+*/
+
+#include "shell.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+char *get_env_value(shell_t *shell, const char *var)
+{
+    int len;
+    int i = 0;
+
+    if (!shell || !var)
+        return NULL;
+    len = my_strlen(var);
+    while (i < shell->env_size) {
+        if (my_strncmp(shell->env_array[i], var, len) == 0 &&
+            shell->env_array[i][len] == '=') {
+            return shell->env_array[i] + len + 1;
+        }
+        i++;
+    }
+    return NULL;
+}
