8c20382e56f4e11c15a75793085083c1ac9074a6
Lukas R. <lukas.renaud@epitech.eu>
2025-04-22 15:06:06 +0200

    fix(parser): simplify null checks in and_or_error_handling function

--- src/parser/error_handling/error_handling_and_or.c
+++ src/parser/error_handling/error_handling_and_or.c
@@ -14,7 +14,7 @@ int and_or_error_handling(ast_node_t *node)
 {
     int status = 0;
 
-    if (node == NULL) {
+    if (!node) {
         fprintf(stderr, "Error: Node is NULL\n");
         return -1;
     }
@@ -22,16 +22,16 @@ int and_or_error_handling(ast_node_t *node)
         fprintf(stderr, "Error: Invalid node type\n");
         return -1;
     }
-    if (node->data.binop.left == NULL || node->data.binop.right == NULL) {
+    if (!node->data.binop.left || !node->data.binop.right) {
         fprintf(stderr, "Invalid null command.\n");
         return -1;
     }
-    if (node->data.binop.left != NULL) {
+    if (node->data.binop.left) {
         status = ast_error_handling(node->data.binop.left);
         if (status != 0)
             return status;
     }
-    if (node->data.binop.right != NULL)
+    if (node->data.binop.right)
         status = ast_error_handling(node->data.binop.right);
     return 0;
 }
