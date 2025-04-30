62892f1287292fbdf58c8aa35e3e0c69c6f4a469
Yannislrg <yannis.lorang@epitech.eu>
2025-04-30 12:20:39 +0200

    refactor(pipe): simplify pipe handling and error management

--- src/command_execution/pipe/execute_pipe_command.c
+++ src/command_execution/pipe/execute_pipe_command.c
@@ -15,14 +15,6 @@
 #include <string.h>
 #include <signal.h>
 
-static void close_pipes(int pipes[][2], int count)
-{
-    for (int i = 0; i < count; i++) {
-        close(pipes[i][0]);
-        close(pipes[i][1]);
-    }
-}
-
 static int execute_child_command(command_info_t *command_info, int i,
     shell_t *shell_var)
 {
@@ -45,37 +37,78 @@ static int execute_child_command(command_info_t *command_info, int i,
     return 1;
 }
 
-static void handle_child_process(int i, command_info_t *command_info,
-    struct shell_s *shell_var)
+static void setup_child_pipes(int prev_pipe[2], int curr_pipe[2])
 {
-    if (i > 0)
-        dup2(command_info->pipes[i - 1][0], STDIN_FILENO);
-    if (i < command_info->command_count - 1)
-        dup2(command_info->pipes[i][1], STDOUT_FILENO);
-    close_pipes(command_info->pipes, command_info->command_count - 1);
-    execute_child_command(command_info, i, shell_var);
-    exit(1);
+    if (prev_pipe[0] != -1) {
+        dup2(prev_pipe[0], STDIN_FILENO);
+        close(prev_pipe[0]);
+    }
+    if (prev_pipe[1] != -1)
+        close(prev_pipe[1]);
+    if (curr_pipe[1] != -1) {
+        dup2(curr_pipe[1], STDOUT_FILENO);
+        close(curr_pipe[1]);
+    }
+    if (curr_pipe[0] != -1)
+        close(curr_pipe[0]);
+}
+
+static void close_parent_pipes(int prev_pipe[2])
+{
+    if (prev_pipe[0] != -1)
+        close(prev_pipe[0]);
+    if (prev_pipe[1] != -1)
+        close(prev_pipe[1]);
+}
+
+static int create_pipe_if_needed(int curr_pipe[2], int prev_pipe[2], int i,
+    int command_count)
+{
+    curr_pipe[0] = -1;
+    curr_pipe[1] = -1;
+    if (i < command_count - 1) {
+        if (pipe(curr_pipe) == -1) {
+            perror("pipe");
+            close_parent_pipes(prev_pipe);
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static int handle_fork_error(int prev_pipe[2], int curr_pipe[2])
+{
+    perror("fork");
+    close_parent_pipes(prev_pipe);
+    if (curr_pipe[0] != -1)
+        close(curr_pipe[0]);
+    if (curr_pipe[1] != -1)
+        close(curr_pipe[1]);
+    return -1;
 }
 
 static int setup_pipes_and_fork(command_info_t *command_info,
     struct shell_s *shell_var)
 {
-    for (int i = 0; i < command_info->command_count - 1; i++) {
-        if (pipe(command_info->pipes[i]) == -1) {
-            perror("pipe");
-            return -1;
-        }
-    }
+    int prev_pipe[2] = {-1, -1};
+    int curr_pipe[2];
+
     for (int i = 0; i < command_info->command_count; i++) {
-        command_info->pids[i] = fork();
-        if (command_info->pids[i] == -1) {
-            perror("fork");
-            close_pipes(command_info->pipes, command_info->command_count - 1);
+        if (create_pipe_if_needed(curr_pipe, prev_pipe, i,
+            command_info->command_count) == -1)
             return -1;
+        command_info->pids[i] = fork();
+        if (command_info->pids[i] == -1)
+            return handle_fork_error(prev_pipe, curr_pipe);
+        if (command_info->pids[i] == 0) {
+            setup_child_pipes(prev_pipe, curr_pipe);
+            execute_child_command(command_info, i, shell_var);
         }
-        if (command_info->pids[i] == 0)
-            handle_child_process(i, command_info, shell_var);
+        close_parent_pipes(prev_pipe);
+        prev_pipe[0] = curr_pipe[0];
+        prev_pipe[1] = curr_pipe[1];
     }
+    close_parent_pipes(prev_pipe);
     return 0;
 }
 
@@ -138,7 +171,6 @@ int execute_pipe(ast_node_t *node, struct shell_s *shell_var)
         cleanup_command_info(command_info);
         return -1;
     }
-    close_pipes(command_info->pipes, command_info->command_count - 1);
     last_status = wait_for_children(command_info, shell_var);
     cleanup_command_info(command_info);
     return last_status;
