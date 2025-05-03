a7a604bf91081d113caef48da9548596921631a8
Lukas R. <lukas.renaud@epitech.eu>
2025-05-03 19:12:15 +0200

    feat(alias): implement free_alias function for memory management of aliases

---
+++ src/alias/free_alias.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** 42sh [WSL: Ubuntu-24.04]
+** File description:
+** free_alias
+*/
+
+#include <stdlib.h>
+#include "alias.h"
+
+void free_alias(alias_t *alias)
+{
+    if (!alias)
+        return;
+    for (int i = 0; alias->info[i] != NULL; i++) {
+        if (alias->info[i]->name)
+            free(alias->info[i]->name);
+        if (alias->info[i]->value)
+            free(alias->info[i]->value);
+        if (alias->info[i])
+            free(alias->info[i]);
+    }
+    free(alias);
+}
