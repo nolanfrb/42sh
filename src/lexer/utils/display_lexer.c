661af528ed395703e794a82873494111accbd63c
Nolan <nolan.fribault@gmail.com>
2025-05-05 23:29:55 +0200

    refactor(lexer): improve variable declaration style in lexer_print_tokens function

--- src/lexer/utils/display_lexer.c
+++ src/lexer/utils/display_lexer.c
@@ -11,13 +11,16 @@
 
 void lexer_print_tokens(lexer_t *lexer)
 {
+    chain_node_t *current;
+    token_t *token;
+
     if (lexer == NULL || lexer->tokens == NULL) {
         fprintf(stderr, "Lexer or tokens are NULL\n");
         return;
     }
-    chain_node_t *current = lexer->tokens->head;
+    current = lexer->tokens->head;
     while (current != NULL) {
-        token_t *token = (token_t *)current->data;
+        token = (token_t *)current->data;
         if (token != NULL) {
             printf("Token type: %d, value: %s\n", token->type, token->value);
         }
