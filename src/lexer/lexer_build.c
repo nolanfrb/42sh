fc2851f9148ca91848fea3fea7361ea5e7af15a1
Nolan <nolan.fribault@gmail.com>
2025-05-05 23:29:44 +0200

    refactor(lexer): simplify special character handling and expose handle_word function

--- src/lexer/lexer_build.c
+++ src/lexer/lexer_build.c
@@ -10,7 +10,7 @@
 #include "lexer.h"
 #include <stdio.h>
 
-static int handle_word(lexer_t *lexer)
+int handle_word(lexer_t *lexer)
 {
     int len = lexer->pos - lexer->start;
     char *word = malloc(sizeof(char) * (len + 1));
@@ -27,63 +27,18 @@ static int handle_word(lexer_t *lexer)
     return 0;
 }
 
-static int handle_pipe(lexer_t *lexer)
-{
-    if (lexer->input[lexer->pos + 1] == '|') {
-        lexer->pos++;
-        return lexer_add_token(lexer, TOKEN_OR, "||");
-    }
-    return lexer_add_token(lexer, TOKEN_PIPE, "|");
-}
-
-static int handle_and(lexer_t *lexer)
-{
-    if (lexer->input[lexer->pos + 1] == '&') {
-        lexer->pos++;
-        return lexer_add_token(lexer, TOKEN_AND, "&&");
-    }
-    lexer->pos++;
-    return handle_word(lexer);
-}
-
-static int handle_redirect_in(lexer_t *lexer)
-{
-    if (lexer->input[lexer->pos + 1] == '<') {
-        lexer->pos++;
-        return lexer_add_token(lexer, TOKEN_HEREDOC, "<<");
-    }
-    return lexer_add_token(lexer, TOKEN_REDIRECT_IN, "<");
-}
-
-static int handle_redirect_out(lexer_t *lexer)
-{
-    if (lexer->input[lexer->pos + 1] == '>') {
-        lexer->pos++;
-        return lexer_add_token(lexer, TOKEN_APPEND_OUT, ">>");
-    }
-    return lexer_add_token(lexer, TOKEN_REDIRECT_OUT, ">");
-}
-
 static int handle_special_char(lexer_t *lexer)
 {
     char c = lexer->input[lexer->pos];
+    const special_char_handler_t *handler = SPECIAL_CHARS;
 
     if (lexer->start != lexer->pos && handle_word(lexer) != 0)
         return -1;
     lexer->start = lexer->pos;
-    switch (c) {
-        case '|':
-            return handle_pipe(lexer);
-        case '&':
-            return handle_and(lexer);
-        case ';':
-            return lexer_add_token(lexer, TOKEN_SEMICOLON, ";");
-        case '<':
-            return handle_redirect_in(lexer);
-        case '>':
-            return handle_redirect_out(lexer);
-        default:
-            break;
+    while (handler->symbol != '\0') {
+        if (handler->symbol == c)
+            return handler->handler(lexer);
+        handler++;
     }
     return 0;
 }
@@ -96,7 +51,9 @@ static int process_char(lexer_t *lexer)
         if (handle_special_char(lexer) != 0)
             return -1;
         lexer->start = lexer->pos + 1;
-    } else if (is_whitespace(c)) {
+        return 0;
+    }
+    if (is_whitespace(c)) {
         if (lexer->start != lexer->pos && handle_word(lexer) != 0)
             return -1;
         lexer->start = lexer->pos + 1;
@@ -108,7 +65,6 @@ int lexer_tokenize(lexer_t *lexer)
 {
     lexer->pos = 0;
     lexer->start = 0;
-
     while (lexer->input[lexer->pos] != '\0') {
         if (process_char(lexer) != 0)
             return -1;
@@ -122,6 +78,7 @@ int lexer_tokenize(lexer_t *lexer)
 lexer_t *lexer_build(const char *input)
 {
     lexer_t *lexer = lexer_init(input);
+
     if (!lexer)
         return NULL;
     lexer_tokenize(lexer);
