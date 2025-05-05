4206f3679d502d755770101ac1d2203afff8fd48
Nolan <nolan.fribault@gmail.com>
2025-05-05 23:28:13 +0200

    feat(lexer): add token types for left and right parentheses and update special character handling

--- include/lexer.h
+++ include/lexer.h
@@ -22,6 +22,8 @@ typedef enum {
     TOKEN_REDIRECT_OUT,
     TOKEN_APPEND_OUT,
     TOKEN_HEREDOC,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
     TOKEN_END
 } token_type_t;
 
@@ -42,6 +44,13 @@ typedef struct {
     bool in_backslash;
 } lexer_t;
 
+typedef struct {
+    char symbol;
+    int (*handler)(lexer_t *);
+} special_char_handler_t;
+
+extern const special_char_handler_t SPECIAL_CHARS[];
+
 lexer_t *lexer_init(const char *input);
 void free_lexer(lexer_t *lexer);
 lexer_t *lexer_build(const char *input);
@@ -52,6 +61,16 @@ int lexer_add_token(lexer_t *lexer, token_type_t type, const char *value);
 int lexer_token_count(lexer_t *lexer);
 
 void lexer_print_tokens(lexer_t *lexer);
+
+// handling special characters
+int handle_word(lexer_t *lexer);
+int handle_pipe(lexer_t *lexer);
+int handle_and(lexer_t *lexer);
+int handle_redirect_in(lexer_t *lexer);
+int handle_redirect_out(lexer_t *lexer);
+int handle_semicolon(lexer_t *lexer);
+int handle_left_parenthesis(lexer_t *lexer);
+int handle_right_parenthesis(lexer_t *lexer);
 
 // Utility functions
 bool is_special_char(char *str);
